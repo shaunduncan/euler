e0a6abfc306409f2bd3c35e7690bd58d6c256f1d
Shaun Duncan <shaun.duncan@gmail.com>
2014-01-14 12:35:14 -0500

    Problem 15 using pascals triangle

--- 015.py
+++ 015.py
@@ -1,4 +1,24 @@
-segments = (2 * pow(20, 2)) + (2 * 20)
-paths = segments / 2
+# Pascal's triangle. The nth middle element. Generate a full triangle until
+# we have the 20th middle element
+rows = {}
+results = []
+i = 0
 
-print segments, path
+while len(results) <= 20:
+    rows[i] = [1]
+
+    if i > 0:
+        for j in xrange(1, i):
+            try:
+                rows[i].append(rows[i-1][j-1] + rows[i-1][j])
+            except IndexError:
+                rows[i].append(1)
+
+        rows[i].append(1)
+
+    if len(rows[i]) % 2 == 1:
+        results.append(rows[i])
+
+    i += 1
+
+print max(results[-1])
